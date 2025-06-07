284f74d5e487d3f6ddef322a1b6b5ef9991ad220
“Nahuel <nahuelguerra56b@gmail.com>
2025-06-07 11:54:32 -0300

    fix: remove commented configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Esto desactiva ESLint durante los builds de producción
   },
-  /* otras configuraciones... */
 };
 
 export default nextConfig;
