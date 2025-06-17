5ddb3d26b00bf8ab49bd03d55c6d9f408a7cf3fb
“Nahuel <nahuelguerra56b@gmail.com>
2025-06-17 18:27:03 -0300

    feat: refactor CORS headers in GET and OPTIONS endpoints for consistency

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,29 +1,35 @@
 import { getCourses } from "@/dal/Courses";
 import { NextResponse } from "next/server";
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "http://localhost:3000",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type",
+};
+
 export async function GET(req: Request) {
   try {
     const courses = await getCourses();
 
     return new NextResponse(JSON.stringify(courses), {
       status: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*", 
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type",
-      },
+      headers: corsHeaders,
     });
   } catch (error) {
     console.error("Error al obtener cursos:", error);
-
     return new NextResponse(
       JSON.stringify({ error: "Error interno del servidor" }),
       {
         status: 500,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
+        headers: corsHeaders,
       }
     );
   }
+}
+
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 204,
+    headers: corsHeaders,
+  });
 }
